5cc1e512fb1890c2e2cf05572200c73cf9c28fec
sdevaraj <santhoshkumar.devaraj@gmail.com>
2016-04-05 15:19:56 -0700

    Landing screen improvements

--- app/src/main/java/io/sdevaraj/goals/ActivityMain.java
+++ app/src/main/java/io/sdevaraj/goals/ActivityMain.java
@@ -2,12 +2,17 @@ package io.sdevaraj.goals;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
