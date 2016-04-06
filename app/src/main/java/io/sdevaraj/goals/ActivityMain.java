01768539aa299a1849ee85cbc4ff62f5ee90fd73
sdevaraj <santhoshkumar.devaraj@gmail.com>
2016-04-05 23:03:51 -0700

    Added Glide library for image loading.

--- app/src/main/java/io/sdevaraj/goals/ActivityMain.java
+++ app/src/main/java/io/sdevaraj/goals/ActivityMain.java
@@ -1,18 +1,43 @@
 package io.sdevaraj.goals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Button eButton;
+
+    private View.OnClickListener mBtnListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(ActivityMain.this, "Button is clicked", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBackgroundImage();
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        eButton = (Button) findViewById(R.id.iv_button);
         setSupportActionBar(mToolbar);
+        eButton.setOnClickListener(mBtnListener);
+    }
+
+    private void initBackgroundImage() {
+        ImageView backgroundImageView = (ImageView) findViewById(R.id.id_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(backgroundImageView);
     }
 }
