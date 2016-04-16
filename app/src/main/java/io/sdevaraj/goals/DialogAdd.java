51cb82ef92f50661d7fd45850854041857fbd885
sdevaraj <santhoshkumar.devaraj@gmail.com>
2016-04-16 11:36:07 -0700

    Variable name change

--- app/src/main/java/io/sdevaraj/goals/DialogAdd.java
+++ app/src/main/java/io/sdevaraj/goals/DialogAdd.java
@@ -54,15 +54,15 @@ public class DialogAdd extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View fragmentView = inflater.inflate(R.layout.dialog_add, container, false);
-        mInputWhat = (EditText) fragmentView.findViewById(R.id.et_drop);
-        mBtnClose = (ImageButton) fragmentView.findViewById(R.id.btn_close);
-        mInputWhen = (DatePicker) fragmentView.findViewById(R.id.bpv_date);
-        mBtnAdd = (Button) fragmentView.findViewById(R.id.btn_add);
+        View view = inflater.inflate(R.layout.dialog_add, container, false);
+        mInputWhat = (EditText) view.findViewById(R.id.et_drop);
+        mBtnClose = (ImageButton) view.findViewById(R.id.btn_close);
+        mInputWhen = (DatePicker) view.findViewById(R.id.bpv_date);
+        mBtnAdd = (Button) view.findViewById(R.id.btn_add);
 
         mBtnClose.setOnClickListener(mBtnClickListener);
         mBtnAdd.setOnClickListener(mBtnClickListener);
 
-        return fragmentView;
+        return view;
     }
 }
